0e06f006f156810274cff5b84df9519a3ec9f784
ruderude <rude1979@gmail.com>
2021-03-16 00:14:46 +0900

    ちょこちょこ改造

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,14 +2,25 @@ module.exports = {
   siteMetadata: {
     title: `Pandas Eating Lots`,
     description: `文字文字文字moji`,
-    hogehoge: `fugafuga`,
-    kunshi: {
-      name: '訓志',
-      age: 42,
-      job: "エンジニア"
-    }
+    author: `gatsbyjs`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
--- src/pages/kunshi.js
+++ src/pages/kunshi.js
@@ -6,14 +6,10 @@ export default function Kunshi({ data }) {
   return (
     <Layout>
         <h1>Kunshi {data.site.siteMetadata.title}</h1>
-        <h1>Hoge {data.site.siteMetadata.hogehoge}</h1>
         <p>
                 We're the only site running on your computer dedicated to showing the
                 best photos and videos of pandas eating lots of food.
         </p>
-        <h1>name: {data.site.siteMetadata.kunshi.name}</h1>
-        <h1>age: {data.site.siteMetadata.kunshi.age}</h1>
-        <h1>job: {data.site.siteMetadata.kunshi.job}</h1>
     </Layout>
   )
 }
@@ -22,13 +18,7 @@ export const query = graphql`
   query {
     site {
         siteMetadata {
-            kunshi {
-                name
-                age
-                job
-            }
             title
-            hogehoge
         }
     }
   }
